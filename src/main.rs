f772e522ec504140167f255ad29f1d6e67f17364
rida-hdj <rida-hdj@proton.me>
2026-09-13 16:51:22 +0100

    add scripts support

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use std::fs;
 use std::io::{self, Write};
-use std::process::{Command};
-
+use std::process::Command;
 
 fn main() {
     println!("welcome to shex");
@@ -8,7 +8,7 @@ fn main() {
         // display the prompt "> "
         print!("> ");
         io::stdout().flush().unwrap();
-        
+
         // take input
         let mut input = String::new();
         if io::stdin().read_line(&mut input).is_err() {
@@ -20,7 +20,7 @@ fn main() {
         if input.is_empty() {
             continue;
         }
-        
+
         // split the input into arguments
         let args: Vec<&str> = input.split_whitespace().collect();
 
@@ -28,13 +28,42 @@ fn main() {
         if args[0] == "exit" {
             break;
         }
-        
+
+        if args[0].ends_with(".sh") {
+            run_script(args[0]);
+            continue;
+        }
+
         // execute commands
-        let mut command = Command::new(args[0]);
-        command.args(&args[1..]);
-        let _ = command.status();
+        execute_command(&args);
     }
     println!("exit");
+}
+
+fn execute_command(args: &[&str]) {
+    let mut command = Command::new(args[0]);
+    command.args(&args[1..]);
+    let _ = command.status();
+}
+
+fn run_script(path: &str) {
+    let content = match fs::read_to_string(path) {
+        Ok(content) => content,
+        Err(log) => {
+            println!("can't read file {} in {}", log, path);
+            return;
+        }
+    };
+    for line in content.lines() {
+        let line = line.trim();
+
+        if line.is_empty() || line.starts_with("#") {
+            continue;
+        }
+
+        let args: Vec<&str> = line.split_whitespace().collect();
+        execute_command(&args);
+    }
 }
 
 // TODO: add "echo > file.txt" and split the code to multiple function
